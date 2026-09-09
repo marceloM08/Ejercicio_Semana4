1c7bd147737ee2d29be32a25933b5e5902744012
marcelo.molina@uamv.edu.ni <marcelo.molina@uamv.edu.ni>
2026-09-09 10:46:44 -0600

    subiendo cambios

--- grades.py
+++ grades.py
@@ -1,3 +1,20 @@
-def read_grades():
-    grades = [85, 92, 76, 60, 45]
-    return grades
+def Read_Grades():
+grades = []
+    try:
+        cantidad = int(input("¿Cuántas notas desea ingresar? "))
+    except ValueError:
+        print("Debe ingresar un número válido.")
+        return notas
+
+    for i in range(cantidad):
+        while True:
+            try:
+                nota = int(input(f"Ingrese la nota #{i+1}: "))
+                if 0 <= nota <= 100:
+                    notas.append(nota)
+                    break
+                else:
+                    print("La nota debe estar entre 0 y 100.")
+            except ValueError:
+                print("Entrada inválida. Ingrese un número entero.")
+    return notas
